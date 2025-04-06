d25ecd8c4de1b24db8a24cd6e3069f16adb56b03
whey <jimmy.hole@mail.ru>
2025-04-06 21:55:45 +0300

    config ref7

--- src/test/java/helpers/CustomAllureListener.java
+++ src/test/java/helpers/CustomAllureListener.java
@@ -3,11 +3,11 @@ package helpers;
 import io.qameta.allure.restassured.AllureRestAssured;
 
 public class CustomAllureListener {
-    private static final AllureRestAssured FILTER = new AllureRestAssured();
+    private static final AllureRestAssured FILTER = new AllureRestAssured()
+            .setRequestTemplate("request.ftl")
+            .setResponseTemplate("response.ftl");
 
     public static AllureRestAssured withCustomTemplates() {
-        FILTER.setRequestTemplate("request.ftl");
-        FILTER.setResponseTemplate("response.ftl");
         return FILTER;
     }
 }
